de877b31d639f692a0a9934ee2293cc32e3dc8f4
Mario randazzo <randazzo.mario6996@gmail.com>
2019-02-24 16:30:15 +0100

    Modified query with CriteriaBuilder

--- plant-watering/plant-watering-web-app/src/main/java/com/randazzo/mario/plantWatering/dao/PlantDAO.java
+++ plant-watering/plant-watering-web-app/src/main/java/com/randazzo/mario/plantWatering/dao/PlantDAO.java
@@ -5,7 +5,13 @@ import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.Root;
 
+import com.randazzo.mario.plantWatering.model.Person;
 import com.randazzo.mario.plantWatering.model.Plant;
 
 @Stateless
@@ -23,21 +29,44 @@ public class PlantDAO {
 	}
 	
 	public Plant findByIdPersonEmail(Long id, String personEmail) {
-		List<Plant> results = em.createQuery("Select p From plant p Where p.id = :id And p.person.email = :email", Plant.class)
-				 				.setParameter("id", id)
-				 				.setParameter("email", personEmail)
-				 				.getResultList();
+		CriteriaBuilder cb = em.getCriteriaBuilder();
 		
+		CriteriaQuery<Plant> criteriaQuery = cb.createQuery(Plant.class);
+		Root<Plant> root = criteriaQuery.from(Plant.class);
+		Join<Plant, Person> personRoot = root.join("person");
+		criteriaQuery
+			.select(root)
+			.where(
+					cb.and(
+							cb.equal(root.get("id"), id), 
+							cb.equal(personRoot.get("email"), personEmail)
+						  )
+				  );
+		
+		TypedQuery<Plant> query = em.createQuery(criteriaQuery);
+		List<Plant> results = query.getResultList();
+
 		if(results.size() > 0) return results.get(0);
 		
 		return null;
 	}
 
 	public List<Plant> findByPersonEmail(String personEmail) {
-		return  em.createQuery("Select p From plant p Where p.person.email = :email", Plant.class)
-				 .setParameter("email", personEmail)
-				 .getResultList();
+		CriteriaBuilder cb = em.getCriteriaBuilder();
 		
+		CriteriaQuery<Plant> criteriaQuery = cb.createQuery(Plant.class);
+		Root<Plant> root = criteriaQuery.from(Plant.class);
+		Join<Plant, Person> personRoot = root.join("person");
+		criteriaQuery
+			.select(root)
+			.where(
+					cb.and(
+							cb.equal(personRoot.get("email"), personEmail)
+						  )
+				  );
+		
+		TypedQuery<Plant> query = em.createQuery(criteriaQuery);
+		return query.getResultList();
 	}
 
 }
